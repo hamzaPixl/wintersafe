f11b3e71fcf12d65a2527bdd1f96eb94485a7e89
Hamza mounir <hamza.pixelle@gmail.com>
2023-11-15 03:30:01 +0100

    Remove cookie banner and update meta tags

--- src/container/layout.js
+++ src/container/layout.js
@@ -2,14 +2,12 @@ import React from 'react'
 import SEO from './seo'
 import Footer from '../components/footer'
 import Header from '../components/header'
-import CookieBanner from '../components/banners/cookie'
 
 export default function Layout({ children }) {
   return (
     <>
       <main>
         <SEO />
-        <CookieBanner />
         <Header />
         {children}
         <Footer />
